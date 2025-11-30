934894d6473de25c6b6ea3f256ab6f986c4b0f2f
JUNGHO-GIT <junghomun00@gmail.com>
2025-11-30 13:41:30 +0900

    2025-11-30 13:41:30

--- .node/gcloud.cjs
+++ .node/gcloud.cjs
@@ -1,173 +1,143 @@
-// server/gcloud.cjs
+// gcloud.cjs
 
 const os = require('os');
-const fs = require('fs');
 const { execSync } = require('child_process');
-const { logger } = require(`./bundle.cjs`);
+const { logger } = require(`./utils.cjs`);
+const { CONFIG } = require(`./env.cjs`);
 
 // 인자 파싱 ------------------------------------------------------------------------------------
 const TITLE = `gcloud.cjs`;
-const winOrLinux = os.platform() === 'win32' ? `win` : `linux`;
+const winOrLinux = os.platform() === `win32` ? `win` : `linux`;
 const argv = process.argv.slice(2);
-const args1 = argv.find(arg => [`--npm`, `--pnpm`, `--yarn`, `--bun`].includes(arg))?.replace(`--`, ``) || ``;
-const args2 = argv.find(arg => [`--deploy`].includes(arg))?.replace(`--`, ``) || ``;
+const args1 = argv.find(arg => [`--npm`, `--pnpm`, `--yarn`, `--bun`].includes(arg))?.replace(`--`, ``) || `bun`;
+const args2 = argv.find(arg => [`--server`, `--client`].includes(arg))?.replace(`--`, ``) || ``;
 
-// 프로젝트 설정 -------------------------------------------------------------------------------
-const CONFIG = {
-	domain: `junghomun.com`,
-	projectName: `JPORTFOLIO`,
-	serverIp: `104.196.212.101`,
-	localPort: {
-		client: 3000,
-		server: 4000
-	},
-	gcs: {
-		bucket: `jungho-bucket`,
-		path: `JPORTFOLIO/SERVER/build.tar.gz`
-	},
-	ssh: {
-		win: {
-			keyPath: `C:\\Users\\jungh\\.ssh\\JKEY`,
-			serviceId: `junghomun00`
-		},
-		linux: {
-			keyPath: `~/ssh/JKEY`,
-			serviceId: `junghomun1234`
-		}
-	}
-};
-
-// env 파일 및 index 파일 수정 ---------------------------------------------------------------------
-const modifyEnvAndIndex = () => {
-	logger(`info`, `.env 및 index.ts 파일 수정 시작`);
-
-	const envFile = fs.readFileSync('.env', 'utf8');
-	const indexFile = fs.readFileSync('index.ts', 'utf8');
-
-	const linesEnv = envFile.split(/\r?\n/);
-	const linesIndex = indexFile.split(/\r?\n/);
-
-	const updatedEnv = linesEnv.map(line => (
-		line.startsWith('CLIENT_URL=') ? (
-			`CLIENT_URL=https://www.${CONFIG.domain}/${CONFIG.projectName}`
-		) : line.startsWith('GOOGLE_CALLBACK_URL=') ? (
-			`GOOGLE_CALLBACK_URL=https://www.${CONFIG.domain}/${CONFIG.projectName}/api/auth/google/callback`
-		) : (
-			line
-		)
-	));
-
-	const updatedIndex = linesIndex.map(line => (
-		line.startsWith(`// const db = process.env.DB_NAME`) ? (
-			`const db = process.env.DB_NAME`
-		) : line.startsWith(`const db = process.env.DB_TEST`) ? (
-			`// const db = process.env.DB_TEST`
-		) : (
-			line
-		)
-	));
-
-	const newEnvFile = updatedEnv.join(os.EOL);
-	const newIndexFile = updatedIndex.join(os.EOL);
-
-	fs.writeFileSync('.env', newEnvFile);
-	fs.writeFileSync('index.ts', newIndexFile);
-
-	logger(`success`, `.env 및 index.ts 파일 수정 완료`);
-};
-
-// 원격 서버에서 스크립트 실행 ---------------------------------------------------------------------
-const runRemoteScript = (winOrLinux=``) => {
-	logger(`info`, `원격 서버 스크립트 실행 시작`);
-
-	const keyPath = winOrLinux === `win` ? (
-		CONFIG.ssh.win.keyPath
-	) : (
-		CONFIG.ssh.linux.keyPath
-	);
-	const serviceId = winOrLinux === `win` ? (
-		CONFIG.ssh.win.serviceId
-	) : (
-		CONFIG.ssh.linux.serviceId
-	);
-
+// 공통 함수 ------------------------------------------------------------------------------------
+const getKeyPath = (platform=``) => platform === `win` ? CONFIG.ssh.win.keyPath : CONFIG.ssh.linux.keyPath;
+const getServiceId = (platform=``) => platform === `win` ? CONFIG.ssh.win.serviceId : CONFIG.ssh.linux.serviceId;
+const runSshCommand = (platform=``, commands=``) => {
+	const keyPath = getKeyPath(platform);
+	const serviceId = getServiceId(platform);
 	const ipAddr = CONFIG.serverIp;
-	const serverPath = `/var/www/${CONFIG.domain}/${CONFIG.projectName}/server`;
 
-	const cmdCd = `cd ${serverPath}`;
-	const cmdGitFetch = `sudo git fetch --all`;
-	const cmdGitReset = `sudo git reset --hard origin/master`;
-	const cmdRmClient = `sudo rm -rf client`;
-	const cmdCh = `sudo chmod -R 755 ${serverPath}`;
-	const cmdStop = `if pm2 describe ${CONFIG.projectName} >/dev/null 2>&1; then sudo pm2 stop ${CONFIG.projectName} && pm2 save; fi`;
-	const cmdNpm = `sudo npm install`;
-	const cmdStart = `sudo pm2 start ecosystem.config.cjs --env production && pm2 save`;
-	const cmdSave = `sleep 5 && sudo pm2 save --force`;
-
-	const sshCommand = winOrLinux === `win` ? (
-		`powershell -Command "ssh -i ${keyPath} ${serviceId}@${ipAddr} '${cmdCd} && ${cmdGitFetch} && ${cmdGitReset} && ${cmdRmClient} && ${cmdCh} && ${cmdStop} && ${cmdNpm} && ${cmdStart} && ${cmdSave}'"`
+	const sshCommand = platform === `win` ? (
+		`powershell -Command "ssh -i ${keyPath} ${serviceId}@${ipAddr} '${commands}'"`
 	) : (
-		`ssh -i ${keyPath} ${serviceId}@${ipAddr} '${cmdCd} && ${cmdGitFetch} && ${cmdGitReset} && ${cmdRmClient} && ${cmdCh} && ${cmdStop} && ${cmdNpm} && ${cmdStart} && ${cmdSave}'`
+		`ssh -i ${keyPath} ${serviceId}@${ipAddr} '${commands}'`
 	);
 
 	logger(`info`, `SSH 명령 실행 중...`);
-	execSync(sshCommand, { stdio: 'inherit' });
-	logger(`success`, `원격 서버 스크립트 실행 완료`);
+	execSync(sshCommand, { "stdio": `inherit` });
 };
 
-// env 파일 및 index 파일 복원 --------------------------------------------------------------------
-const restoreEnvAndIndex = () => {
-	logger(`info`, `.env 및 index.ts 파일 복원 시작`);
+// Client 배포 함수 -----------------------------------------------------------------------------
+const buildProject = () => {
+	logger(`info`, `프로젝트 빌드 시작`);
+	execSync(`${args1} run build`, { "stdio": `inherit` });
+	logger(`info`, `프로젝트 빌드 완료`);
+};
 
-	const envFile = fs.readFileSync('.env', 'utf8');
-	const indexFile = fs.readFileSync('index.ts', 'utf8');
+const compressBuild = () => {
+	logger(`info`, `build 폴더 압축 시작`);
+	execSync(`tar -zcvf build.tar.gz build`, { "stdio": `inherit` });
+	logger(`info`, `build 폴더 압축 완료`);
+};
 
-	const linesEnv = envFile.split(/\r?\n/);
-	const linesIndex = indexFile.split(/\r?\n/);
+const uploadToGCP = () => {
+	logger(`info`, `GCP 업로드 시작`);
+	const gcpPath = `gs://${CONFIG.gcp.bucket}/${CONFIG.gcp.path}`;
+	execSync(`gcloud storage cp build.tar.gz ${gcpPath}`, { "stdio": `inherit` });
+	logger(`info`, `GCP 업로드 완료: ${gcpPath}`);
+};
 
-	const updatedEnv = linesEnv.map(line => (
-		line.startsWith('CLIENT_URL=') ? (
-			`CLIENT_URL=http://localhost:${CONFIG.localPort.client}/${CONFIG.projectName}`
-		) : line.startsWith('GOOGLE_CALLBACK_URL=') ? (
-			`GOOGLE_CALLBACK_URL=http://localhost:${CONFIG.localPort.server}/${CONFIG.projectName}/api/auth/google/callback`
-		) : (
-			line
-		)
-	));
+const deleteBuildTar = (platform=``) => {
+	logger(`info`, `build.tar.gz 삭제 시작`);
+	const command = platform === `win` ? `del build.tar.gz` : `rm -rf build.tar.gz`;
+	execSync(command, { "stdio": `inherit` });
+	logger(`info`, `build.tar.gz 삭제 완료`);
+};
 
-	const updatedIndex = linesIndex.map(line => (
-		line.startsWith(`const db = process.env.DB_NAME`) ? (
-			`// const db = process.env.DB_NAME`
-		) : line.startsWith(`// const db = process.env.DB_TEST`) ? (
-			`const db = process.env.DB_TEST`
-		) : (
-			line
-		)
-	));
+const runClientRemoteScript = (platform=``) => {
+	logger(`info`, `원격 서버 클라이언트 배포 스크립트 실행 시작`);
 
-	const newEnvFile = updatedEnv.join(os.EOL);
-	const newIndexFile = updatedIndex.join(os.EOL);
+	const basePath = `/var/www/${CONFIG.domain}/${CONFIG.projectName}`;
+	const clientPath = `${basePath}/client`;
+	const gcpPath = `gs://${CONFIG.gcp.bucket}/${CONFIG.gcp.path}`;
 
-	fs.writeFileSync('.env', newEnvFile);
-	fs.writeFileSync('index.ts', newIndexFile);
+	const commands = [
+		`cd ${basePath}`,
+		`sudo rm -rf client`,
+		`sudo mkdir -p client`,
+		`cd ${clientPath}`,
+		`sudo gcloud storage cp ${gcpPath} .`,
+		`sudo tar -zvxf build.tar.gz --strip-components=1`,
+		`sudo rm build.tar.gz`,
+		`sudo chmod -R 755 ${clientPath}`,
+		`sudo systemctl restart nginx`
+	].join(` && `);
 
-	logger(`success`, `.env 및 index.ts 파일 복원 완료`);
+	runSshCommand(platform, commands);
+	logger(`info`, `원격 서버 클라이언트 배포 스크립트 실행 완료`);
+};
+
+// Server 배포 함수 -----------------------------------------------------------------------------
+const runGitPush = () => {
+	logger(`info`, `git push 명령어 실행 시작`);
+	execSync(`${args1} .node/git.cjs --${args1} --push`, { "stdio": `inherit` });
+	logger(`info`, `git push 명령어 실행 완료`);
+};
+
+const runServerRemoteScript = (platform=``) => {
+	logger(`info`, `원격 서버 서버 배포 스크립트 실행 시작`);
+
+	const serverPath = `/var/www/${CONFIG.domain}/${CONFIG.projectName}/server`;
+
+	const commands = [
+		`cd ${serverPath}`,
+		`sudo git fetch --all`,
+		`sudo git reset --hard ${CONFIG.git.deploy.resetBranch}`,
+		`sudo rm -rf client`,
+		`sudo chmod -R 755 ${serverPath}`,
+		`if pm2 describe ${CONFIG.projectName} >/dev/null 2>&1; then sudo pm2 stop ${CONFIG.projectName} && pm2 save; fi`,
+		`sudo npm install`,
+		`sudo pm2 start ecosystem.config.cjs --env production && pm2 save`,
+		`sleep 5 && sudo pm2 save --force`
+	].join(` && `);
+
+	runSshCommand(platform, commands);
+	logger(`info`, `원격 서버 서버 배포 스크립트 실행 완료`);
 };
 
 // 실행 ---------------------------------------------------------------------------------------
 (() => {
 	logger(`info`, `스크립트 실행: ${TITLE}`);
-	logger(`info`, `전달된 인자 1 : ${args1 || 'none'}`);
-	logger(`info`, `전달된 인자 2 : ${args2 || 'none'}`);
-	logger(`info`, `운영체제 : ${winOrLinux}`);
+	logger(`info`, `전달된 인자 1 (패키지 매니저): ${args1}`);
+	logger(`info`, `전달된 인자 2 (배포 대상): ${args2 || `none`}`);
+	logger(`info`, `운영체제: ${winOrLinux}`);
+
+	const isValidArgs = args2 === `client` || args2 === `server`;
+	!isValidArgs ? (
+		logger(`error`, `배포 대상을 지정해주세요. (--server 또는 --client)`),
+		process.exit(1)
+	) : null;
 
 	try {
-		args2.includes(`--deploy`) && (() => {
-			modifyEnvAndIndex();
-			runRemoteScript(winOrLinux);
-			restoreEnvAndIndex();
-			logger(`success`, `GCloud 배포 프로세스 완료`);
+		args2 === `client` && (() => {
+			logger(`info`, `클라이언트 배포 프로세스 시작`);
+			buildProject();
+			compressBuild();
+			uploadToGCP();
+			deleteBuildTar(winOrLinux);
+			runClientRemoteScript(winOrLinux);
+			logger(`info`, `클라이언트 배포 프로세스 완료`);
 		})();
+		args2 === `server` && (() => {
+			logger(`info`, `서버 배포 프로세스 시작`);
+			runGitPush();
+			runServerRemoteScript(winOrLinux);
+			logger(`info`, `서버 배포 프로세스 완료`);
+		})();
+		logger(`info`, `전체 배포 프로세스 완료`);
 		process.exit(0);
 	}
 	catch (e) {
